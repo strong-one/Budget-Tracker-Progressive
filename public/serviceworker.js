d8f1e43837c809e5d680925e81d31ddc20b67fd0
Nick Strong <nstrong92@yahoo.com>
2021-06-14 21:24:35 -0600

    reading about service workers and taking notes in file

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,1 +1,12 @@
+// will need to cache data requests and pages in this file
+
 // run things offline
+// a JS script that gets registered with the browser
+// stays registered with browser even when offline
+// js script that can load even with no connection
+// when service worker is introduced, it adds another level that intercepts the request and decides what to do with the request.
+
+// will need to cache data requests and pages in this file
+// service workers are terminated when not in use. need https when pushing to remote server
+// life cycle events -- register worker -- install event -- activate event
+// functional events fetch -- sync
